ba432cb4c5590fcd597eedec8bd56bec77a8f7ee
Harshit Malik <harshit.malik999@gmail.com>
2018-11-07 00:36:58 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include "AVLtree.hpp"
+#include "functions.h"
+using namespace std;
+
+int main(){
+	char choice;
+	AVLtree avlTree;
+	cout<<"\n===== DICTIONARY =====\n\n";
+	cout<<"Read word meaning file ? (y/n)\n";
+	cin>>choice;
+	if( choice == 'y' || choice == 'Y' ){
+		readFileData(avlTree);
+		mainMenu(avlTree);
+	}
+	else
+		mainMenu(avlTree);
+return 0;
+}
